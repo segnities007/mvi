425b47f420a54d88dd591fe4a6d59d5d8949deb7
seg <segnities007@gmail.com>
2025-10-11 09:33:09 +0900

    modify UiEffect

--- app/src/main/java/com/segnities007/mvi/Reducer.kt
+++ app/src/main/java/com/segnities007/mvi/Reducer.kt
@@ -7,8 +7,5 @@ package com.segnities007.mvi
  * @param Intent ユーザーの意図を表す型
  */
 interface Reducer<State : UiState, Intent : UiIntent> {
-    /**
-     * Intentを処理して新しい状態を生成する
-     */
     fun reduce(state: State, intent: Intent): State
 }
--- app/src/main/java/com/segnities007/mvi/UiEffect.kt
+++ app/src/main/java/com/segnities007/mvi/UiEffect.kt
@@ -2,8 +2,6 @@ package com.segnities007.mvi
 
 /**
  * 一度きりのイベント（副作用）を表現する
- *
  * ナビゲーション、Snackbar表示など
- * sealed interfaceとして定義すること
  */
-sealed interface UiEffect
+interface UiEffect
